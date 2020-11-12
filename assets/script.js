77aaeaeb115e5132d2ebf9f484525abed8b70bbe
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-11-11 16:16:01 -0800

    adjusted variable to display current weather

--- assets/script.js
+++ assets/script.js
@@ -15,27 +15,42 @@
 
 var searchButton = $('#buttonSearch');
 var citySearch = $('#citySearch');
+var search = [];
 
-
-searchButton.click(function(event) {
+searchButton.click(function createSearch(event) {
     event.preventDefault();
     var submittedSearch = citySearch.submit();
+    var userChoice = submittedSearch.val().toLowerCase();
 
-    var search = submittedSearch.val().toLowerCase();
+    search.push(userChoice);
 
-    $('#cityHistory').text(citySearch.val());
-
-    var APIKey = "d5866c2d5c0e76c2380895bf8574fe70";
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ search +"&appid=" + APIKey;
+    $('#cityHistory').text(citySearch.val()); //make a button, display local storage
 
     console.log(search);
+
+    displayCurrent();
+ });
+
+
+  function displayCurrent() {
+
+   for (i = 0; i < search.length; i++) { 
+     var APIKey = "d5866c2d5c0e76c2380895bf8574fe70";
+     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ search[i] +"&appid=" + APIKey;
+
     console.log(queryURL);
 
-     $.ajax({
-         url: queryURL,
-         method: "GET"
-     }).then(function(response){
-         console.log(response);
-}) 
-});
+       $.ajax({
+           url: queryURL,
+           method: "GET"
+       }).then(function(response){
+           console.log(response);
+           $('#current-city').text(response.city.name); //use moment.js for date
+           $('#tempToday').text(response.list[0].main.temp);
+           $('#humToday').text(response.list[0].main.humidity);
+           $('#wind-speed').text(response.list[0].wind.speed);
+           $('#uv').text(response.city.name);
+         }) 
 
+        }
+  }
